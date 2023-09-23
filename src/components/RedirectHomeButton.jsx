4c9f3bdad73095d93d063f8c419011ed2f72de93
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-09-23 12:00:59 +1000

    feat: Home button in contact page

---
+++ src/components/RedirectHomeButton.jsx
@@ -0,0 +1,28 @@
+// Show a button
+// People can click to choose when they get redirected
+// Keep track of state so we can have a variable for control flow of the redirection
+// ONLY render the <Navigate /> component when the variable is true
+
+import { Component } from "react";
+import { Navigate } from "react-router-dom";
+
+export default class RedirectHomeButton extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            shouldRedirect: false
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={() => this.setState({shouldRedirect: true})}>
+                    Return Home
+                </button>
+                {this.state.shouldRedirect && <Navigate to="/" />}
+            </div>
+        )
+    }
+}
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,8 +1,14 @@
+// import { Navigate } from "react-router-dom";
+import RedirectHomeButton from "../components/RedirectHomeButton"
+
 
 export default function ContactPage(props){
 	return(
 		<div>
 			<h1>Contact Us</h1>
+
+            {/* <Navigate to="/" /> */}
+            <RedirectHomeButton />
 		</div>
 	)
 }
